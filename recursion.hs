c8cc64d85391d99f5b4e3be79f7ab2197857d134
Arinde Eniola <eniolaarinde1@gmail.com>
2016-03-20 22:38:10 +0100

    recursion

---
+++ recursion.hs
@@ -0,0 +1,11 @@
+module Test where
+
+-- Recursion
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
