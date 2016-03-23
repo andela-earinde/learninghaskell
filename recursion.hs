090561fbb87445b69eea543abacf61146034b6ef
Arinde Eniola <eniolaarinde1@gmail.com>
2016-03-23 11:35:24 +0100

    apply some changes

--- recursion.hs
+++ recursion.hs
@@ -9,3 +9,20 @@ maximum' (x:xs)
     | x > maxTail = x
     | otherwise = maxTail
     where maxTail = maximum' xs
+
+-- Using max
+maximum1 :: (Ord a) => [a] -> a
+maximum1 [] = error "maximum of empty list"
+maximum1 [x] = x
+maximum1 (x:xs) = max x (maximum1 xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' _ [] = []
+take' i (x:xs)
+    | i < 1 = []
+    | otherwise = x:take' (i-1) xs
