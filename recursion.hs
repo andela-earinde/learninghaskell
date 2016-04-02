482be66c0854a50140eff55aba14fa06ec90dbfe
Arinde Eniola <eniolaarinde1@gmail.com>
2016-04-02 21:53:09 +0100

    add some more solutions in recursion

--- recursion.hs
+++ recursion.hs
@@ -41,3 +41,16 @@ elem' _ [] = False
 elem' a (x:xs)
     | a == x = True
     | otherwise = a `elem'` xs
+
+length' :: [a] -> Int
+length' [] = 0
+length' (x:xs) = 1 + length' xs
+
+-- Implementing quicksort algorithm
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in  smallerSorted ++ [x] ++ biggerSorted
+
